460814fde330d1b43634cf8d34db7e7455868599
Rahul Mody <rmody3@gmail.com>
2017-01-26 17:50:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(number)
+  if  number <= 1
+    prime = false
+  elsif (number**0.5).floor.to_i == 1
+    prime = true
+  else
+    for i in 2..(number**0.5).floor.to_i   
+      if number % i == 0
+        prime = false
+        break
+      else
+        prime = true
+      end
+    end
+  end
+  prime
+end
